32d1c44dc8cdd946a746152777fdc5266896de2e
msl <michael.s.lemke@gmail.com>
2024-06-24 07:43:31 -0500

    updated .sh to .ps1

---
+++ CLI scripts/delete_service_principal.ps1
@@ -0,0 +1,16 @@
+# Load the .env file from the parent directory
+$envFilePath = "..\.env"
+
+if (Test-Path $envFilePath) {
+    $envContent = Get-Content $envFilePath | Where-Object { $_ -match "^[^#]" }  # Ignore comment lines
+    foreach ($line in $envContent) {
+        $name, $value = $line -split '=', 2
+        [System.Environment]::SetEnvironmentVariable($name.Trim(), $value.Trim())
+    }
+} else {
+    Write-Output ".env file not found"
+    exit 1
+}
+
+# Now you can use the variables from the .env file
+Write-Output "Database URL: $SERVICE_PRINCIPAL"

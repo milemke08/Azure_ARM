ba1e6c74be5687ce5c2b61bf338a4f925d708515
msl <michael.s.lemke@gmail.com>
2024-06-24 08:05:16 -0500

    debugging deleteing service principal from azure CLI

--- CLI scripts/delete_service_principal.ps1
+++ CLI scripts/delete_service_principal.ps1
@@ -5,8 +5,14 @@ if (Test-Path $envFilePath) {
     $envContent = Get-Content $envFilePath | Where-Object { $_ -match "^[^#]" }  # Ignore comment lines
     foreach ($line in $envContent) {
         $name, $value = $line -split '=', 2
-        [System.Environment]::SetEnvironmentVariable($name.Trim(), $value.Trim())
-        Write-Output $name.Trim(), $value.Trim()
+        # [System.Environment]::SetEnvironmentVariable($name.Trim(), $value.Trim())
+        # Write-Output $name.Trim(), $value.Trim()
+
+          # Check if the name is "SERVICE_PRINCIPAL" and run az ad sp delete if it is
+          if ($name.Trim() -eq "SERVICE_PRINCIPAL") {
+            Write-Output "Deleting Service Principal: " $value.Trim()
+            az ad sp delete --id $value.Trim()
+          }
     }
 } else {
     Write-Output ".env file not found"
@@ -14,4 +20,7 @@ if (Test-Path $envFilePath) {
 }
 
 # Now you can use the variables from the .env file
-Write-Output "SERVICE_PRINCIPAL: $SERVICE_PRINCIPAL"
+# Write-Output "SERVICE_PRINCIPAL: $SERVICE_PRINCIPAL"
+
+# az ad sp list --display-name <service-principal-name> --query "[].{DisplayName:displayName, AppId:appId, ObjectId:objectId}"
+# 

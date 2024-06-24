303a717f45eb024875bf7d60849957150b81da7b
msl <michael.s.lemke@gmail.com>
2024-06-24 07:55:50 -0500

    resolved .ps1 script not executing

--- CLI scripts/delete_service_principal.ps1
+++ CLI scripts/delete_service_principal.ps1
@@ -1,11 +1,12 @@
 # Load the .env file from the parent directory
-$envFilePath = "..\.env"
+$envFilePath = "../.env"
 
 if (Test-Path $envFilePath) {
     $envContent = Get-Content $envFilePath | Where-Object { $_ -match "^[^#]" }  # Ignore comment lines
     foreach ($line in $envContent) {
         $name, $value = $line -split '=', 2
         [System.Environment]::SetEnvironmentVariable($name.Trim(), $value.Trim())
+        Write-Output $name.Trim(), $value.Trim()
     }
 } else {
     Write-Output ".env file not found"
@@ -13,4 +14,4 @@ if (Test-Path $envFilePath) {
 }
 
 # Now you can use the variables from the .env file
-Write-Output "Database URL: $SERVICE_PRINCIPAL"
+Write-Output "SERVICE_PRINCIPAL: $SERVICE_PRINCIPAL"

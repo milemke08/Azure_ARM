38137cac6031c762e848340f827908caaefb4b56
msl <michael.s.lemke@gmail.com>
2024-06-27 13:46:47 -0500

    updated adls script to create storage account for yellow taxi example

--- storage/4 adls_setup.py
+++ storage/4 adls_setup.py
@@ -1,4 +1,5 @@
 import os
+from dotenv import load_dotenv
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.storage import StorageManagementClient
@@ -6,6 +7,7 @@ from azure.mgmt.storage.models import StorageAccountCreateParameters
 
 def main():
     try:
+        load_dotenv()
         # Set up Azure credentials and clients
         credential = DefaultAzureCredential()
         subscription_id = os.getenv('AZURE_SUBSCRIPTION_ID')
@@ -13,13 +15,13 @@ def main():
         storage_client = StorageManagementClient(credential, subscription_id)
 
         # Define parameters
-        resource_group_name = 'myResourceGroup'
-        location = 'eastus'
-        storage_account_name = 'mystrgeactazsdk2_adlsv2'
+        resource_group_name = os.getenv('RESOURCE_GROUP_NAME')
+        location = os.getenv('LOCATION')
+        storage_account_name = os.getenv('STORAGE_ACCOUNT_NAME')
 
         # Create resource group
-        print("Creating resource group...")
-        resource_client.resource_groups.create_or_update(resource_group_name, {'location': location})
+        # print("Creating resource group...")
+        # resource_client.resource_groups.create_or_update(resource_group_name, {'location': location})
 
         # Create storage account
         print("Creating storage account...")
@@ -29,6 +31,7 @@ def main():
             location=location,
             is_hns_enabled=True  # Hierarchical namespace must be enabled for Data Lake Storage Gen2
         )
+        print(storage_account_name)
         storage_client.storage_accounts.begin_create(resource_group_name, storage_account_name, storage_parameters).result()
 
         print("Storage account created successfully!")
